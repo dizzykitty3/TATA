5ef7d8efbfa4d867453bdf377d3af3b0da6729ec
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2026-08-02 22:30:03 +0800

    Initial Commit

---
+++ TATA/TATAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TATAApp.swift
+//  TATA
+//
+//  Created by Theo on 8/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TATAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TATATests/TATATests.swift
@@ -0,0 +1,18 @@
+//
+//  TATATests.swift
+//  TATATests
+//
+//  Created by Theo on 8/2/26.
+//
+
+import Testing
+
+struct TATATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
